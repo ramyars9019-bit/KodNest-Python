7ec55eb33a3cba2af62e0da4e5694e7c78d92208
Ramya <ramyars9019@gmail.com>
2026-08-04 19:59:56 +0530

    changes3

---
+++ track/M02-Intoduction to Python/T01-Introduction to Python and Basic Constucts/src/pseduocode_python.py
@@ -0,0 +1,7 @@
+# Add the required lines here
+
+print("Welcome to KodNest")
+
+print("Python Track Started")
+
+print("Let us begin coding")
---
+++ track/M02-Intoduction to Python/T01-Introduction to Python and Basic Constucts/src/student_details.py
@@ -0,0 +1,29 @@
+# Read and convert the student details
+
+student_name = input()
+
+student_age = input()
+
+course_rating = input()
+
+# Display the values
+
+student_name = str(student_name)
+
+student_age = int(student_age)
+
+course_rating = float(course_rating)
+
+# Display the data types
+
+print(f"Student: {student_name}")
+
+print(f"Age: {student_age}")
+
+print(f"Rating: {course_rating}")
+
+print (type (student_name))
+
+print(type(student_age))
+
+print(type (course_rating))
